79412dccebe417f7fb4159e492776f105f33c5bb
Kristoffer B. Yddal <yddal@outlook.com>
2026-01-26 19:53:24 +0100

    Lagt til studielokasjon, fikset litt i syntax for tekst og gjort slik at alle json filer legger seg i egen mappe for prosessering.

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ import DataExtractor
 
 
 url = "https://fagskolen-viken.no/studier/ledelse/administrativ-koordinator"
-
+url = "https://fagskolen-viken.no/studier/ledelse/praktisk-lederutdanning"
 DataExtractor.extract(url)
