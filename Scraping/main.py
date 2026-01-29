a0681c814d355b7a829a8970512266413bf32ece
Jan Magne Dybvik <jan.m.dybvik@gmail.com>
2026-01-29 13:10:56 +0100

    refactor og kommentering

--- Scraping/main.py
+++ Scraping/main.py
@@ -3,12 +3,16 @@ import DataExtractor
 import time
 
 """
-This is main
+Starter scraping av nettsiden
+1. Hent alle linker til studie sider
+2. Hent ut all data for hvert studi og lagre dem som json filer
 """
 
 if __name__ == "__main__":
-
+    # hent linker
     urls = get_urls()
+    
+    # les ut alle data for studier
     for url in urls:
         DataExtractor.extract(url)
         # vent litt mellom hver forespørsel
